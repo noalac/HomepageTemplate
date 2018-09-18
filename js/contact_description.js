291425818ee95c38e6ad3eaf15ac4f1f92546a47
noalac <noalac@gmail.com>
2018-09-18 15:45:30 -0500

    Updated Homepage Templated

--- js/contact_description.js
+++
@@ -1,9 +0,0 @@
-      function showDescription() {
-        document.getElementById("contact-photo-description").style.opacity = 1;
-        document.getElementById("contact-photo-description").style.filter = "Alpha(opacity=100)";
-      }
-      function hideDescription() {
-        document.getElementById("contact-photo-description").style.opacity = 0;
-        document.getElementById("contact-photo-description").style.filter = "Alpha(opacity=0)";
-      }
-
